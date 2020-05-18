809f7bee30d7db48f8de3fa923403c7488751fff
Marshall <magj2006@gmail.com>
2020-05-18 06:21:04 +0500

    update for remove warning information

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(global_asm)]
-#![feature(asm)]
+#![feature(llvm_asm)]
 
 use core::panic::PanicInfo;
 
@@ -18,14 +18,14 @@ extern "C" fn abort() -> ! {
 global_asm!(include_str!("boot/entry64.asm"));
 
 pub fn console_putchar(ch: u8) {
-    let ret: usize;
+    let _ret: usize;
     let arg0: usize = ch as usize;
     let arg1: usize = 0;
     let arg2: usize = 0;
     let which: usize = 1;
     unsafe {
-        asm!("ecall"
-             : "={x10}" (ret)
+        llvm_asm!("ecall"
+             : "={x10}" (_ret)
              : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
              : "memory"
              : "volatile"
